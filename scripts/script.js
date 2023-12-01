6cfd0f1b362334089890883ccd552810f1beef0b
Ekam <ekamaujla24@gmail.com>
2023-11-30 17:10:33 -0800

    .

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+function navigateTo(page) {
+    if (page === 'home') {
+        window.location.href = './index.html';
+    } else if (page === 'about') {
+        window.location.href = './about.html';
+    } else if (page === 'contact') {
+        window.location.href = './contact.html';
+    } else if (page === 'myresume') {
+        window.location.href = './myresume.html';
+    } else if (page === 'github') {
+        window.location.href = 'https://github.com/EkamAujla';
+    } else if (page === 'instagram') {
+        window.location.href = 'https://www.instagram.com/_.e.k.a.m._/';
+    } else if (page === 'whatsapp') {
+        window.location.href = 'https://wa.me/qr/4JPLCWOBS4SFO1';
+    } else if (page === 'projects') {
+        window.location.href = './myprojects.html';
+    } else if (page === 'education') {
+        window.location.href = './education.html';
+    } else if (page === 'discord') {
+        window.location.href = 'https://discord.com/channels/@me/739160153852608642';
+    } else if (page === 'sylvie') {
+        window.location.href = 'https://github.com/EkamAujla/Sylvie';
+    } else if (page === 'handler') {
+        window.location.href = 'https://github.com/EkamAujla/Multi-Bots';
+    }
+}
